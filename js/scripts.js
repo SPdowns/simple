98c3dbdf284b51ecb9bb2a66ad233eb8511014ec
Sean Downs <spdowns@protonmail.com>
2020-03-24 10:37:13 -0700

    add css cursor change and text-decor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /* this is to toggle a picture by clicking paragraph text*/
   $("p").click(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
