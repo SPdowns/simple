ecc759041930f4757682f875bed7c72ea151f73d
Sean Downs <spdowns@protonmail.com>
2020-03-24 10:59:18 -0700

    change class to id in walrus references

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   /* this is to toggle a picture by clicking paragraph text*/
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
 });
