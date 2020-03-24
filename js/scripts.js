2ff74387df73dffde6fd4741c0bc873678ddb6ee
Sean Downs <spdowns@protonmail.com>
2020-03-24 11:14:04 -0700

    try different elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   /* this is to toggle a picture by clicking paragraph text*/
   $(".clickable").click(function() {
-    $("#walrus-showing").fadeToggle();
-    $("#walrus-hidden").fadeToggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
 });
